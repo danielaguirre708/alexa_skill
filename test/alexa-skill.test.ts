bbac5ce494a97e604c872a5ac53ad6c9d9b8ce8f
daniel.aguirre2 <daniel.aguirre2@nordstrom.com>
2024-05-09 14:35:08 -0500

    Initial Commit

---
+++ test/alexa-skill.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AlexaSkill from '../lib/alexa-skill-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/alexa-skill-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AlexaSkill.AlexaSkillStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
